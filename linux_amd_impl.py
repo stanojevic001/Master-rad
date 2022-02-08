b605c7c041dd57892ed5a116a4305ff88f1a8949
stanojevic001 <stanojevic.stefan023@gmail.com>
2022-02-08 23:39:38 +0100

    Creating linux support

--- linux_amd_impl.py
+++
@@ -1,9 +0,0 @@
-from common_api import CommonAPI, StatusCode
-
-class LinuxAMD_API(CommonAPI):
-    
-    def initialize(self) -> StatusCode:
-        pass
-
-    def finish(self) -> StatusCode:
-        pass
