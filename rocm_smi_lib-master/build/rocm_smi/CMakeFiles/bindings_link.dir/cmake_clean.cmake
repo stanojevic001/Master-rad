cd000785895a4a832d011f0b32c7163741926f57
stanojevic001 <stanojevic.stefan023@gmail.com>
2022-04-05 22:31:30 +0200

    Deleted rocm smi lib

--- rocm_smi_lib-master/build/rocm_smi/CMakeFiles/bindings_link.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bindings_link"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bindings_link.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- rocm_smi_lib-master/build/rocm_smi/CMakeFiles/so-link.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/so-link"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/so-link.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
