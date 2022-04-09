d5e9b09745ebe739abe9bdd9700855c56a510fe5
stanojevic001 <stanojevic.stefan023@gmail.com>
2022-04-09 14:06:42 +0200

    New changes

--- linux_rocm_smi_wrapper.py
+++ linux_rocm_smi_wrapper.py
@@ -1,23 +1,25 @@
 from typing import Any
 from common_api import CommonAPI
 from defines import *
+import rocm_smi
 
 class Linux_ROCm_SMI_Wrapper(CommonAPI):
 
     def initialize() -> StatusCode:
-        pass
+        rocm_smi.initializeRsmi()
+        return StatusCode.SUCCESS
 
     def finish() -> StatusCode:
         pass
 
     def get_number_of_devices() -> int:
-        pass
+        return len(rocm_smi.listDevices())
 
     def initialize_with_flags(self, flags: Any) -> StatusCode:
         pass
 
     def get_driver_version(self) -> str:
-        pass
+        rocm_smi.getVersion()
 
     def get_library_version(self) -> str:
         pass
