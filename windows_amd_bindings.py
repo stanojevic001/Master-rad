0130933e35db286f3f08c0205ae43d374fc5480b
Stanojevic, Stefan <Stefan.Stanojevic@amd.com>
2022-07-07 23:48:54 +0200

    Add new changes for AMD ADL API

--- windows_amd_bindings.py
+++ windows_amd_bindings.py
@@ -1,13 +1,13 @@
 import ctypes
 
-class C_ADLVersionsInfo(ctypes.Structure):
+class ADLVersionsInfo(ctypes.Structure):
     _fields_ = [
         ('strDriverVer', ctypes.c_char * 256),
         ('strCatalystVersion', ctypes.c_char * 256),
         ('strCatalystWebLink', ctypes.c_char * 256)
     ]
 
-class C_AdapterInfoX2(ctypes.Structure):
+class AdapterInfoX2(ctypes.Structure):
     _fields_ = [
         ('iSize', ctypes.c_int),
         ('iAdapterIndex', ctypes.c_int),
@@ -28,6 +28,16 @@ class C_AdapterInfoX2(ctypes.Structure):
         ('iInfoValue', ctypes.c_int)
     ]
 
+class ADLChipSetInfo(ctypes.Structure):
+    __fields__ = [
+        ('iBusType', ctypes.c_int),
+        ('iBusSpeedType', ctypes.c_int),
+        ('iMaxPCIELaneWidth', ctypes.c_int),
+        ('iCurrentPCIELaneWidth', ctypes.c_int),
+        ('iSupportedAGPSpeeds', ctypes.c_int),
+        ('iCurrentAGPSpeed', ctypes.c_int)
+    ]
+
 class Ctypes_ADL():
     adl_lib = None
     functions = {
@@ -35,7 +45,9 @@ class Ctypes_ADL():
         "adl_finish": None,
         "adl_get_number_of_devices": None,
         "adl_get_driver_version": None,
-        "adl_get_adapter_info": None
+        "adl_get_device_adapter_info": None,
+        "adl_get_device_asic_family_type": None,
+        "adl_get_device_chipset_info": None
     }
 
     def __init__(self) -> None:
@@ -44,8 +56,12 @@ class Ctypes_ADL():
         self.adl_lib.initialize.argtypes = []
         self.adl_lib.initialize.restype = ctypes.c_int
         self.functions["adl_initialize"] = self.adl_lib.initialize
+
+        self.adl_lib.finish.argtypes = []
+        self.adl_lib.finish.restype = ctypes.c_int
+        self.functions["adl_finish"] = self.adl_lib.finish
         
-        self.adl_lib.get_driver_version.argtypes = [ctypes.POINTER(C_ADLVersionsInfo)]
+        self.adl_lib.get_driver_version.argtypes = [ctypes.POINTER(ADLVersionsInfo)]
         self.adl_lib.get_driver_version.restype = ctypes.c_int
         self.functions["adl_get_driver_version"] = self.adl_lib.get_driver_version
 
@@ -53,10 +69,14 @@ class Ctypes_ADL():
         self.adl_lib.get_number_of_devices.restype = ctypes.c_int
         self.functions["adl_get_number_of_devices"] = self.adl_lib.get_number_of_devices
 
-        self.adl_lib.get_adapter_info.argtypes = [ctypes.POINTER(ctypes.POINTER(C_AdapterInfoX2))]
-        self.adl_lib.get_adapter_info.restype = ctypes.c_int
-        self.functions["adl_get_adapter_info"] = self.adl_lib.get_adapter_info
+        self.adl_lib.get_device_adapter_info.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.POINTER(AdapterInfoX2))]
+        self.adl_lib.get_device_adapter_info.restype = ctypes.c_int
+        self.functions["adl_get_device_adapter_info"] = self.adl_lib.get_device_adapter_info
 
-        self.adl_lib.finish.argtypes = []
-        self.adl_lib.finish.restype = ctypes.c_int
-        self.functions["adl_finish"] = self.adl_lib.finish
+        self.adl_lib.get_device_asic_family_type.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int)]
+        self.adl_lib.get_device_asic_family_type.restype = ctypes.c_int
+        self.functions["adl_get_device_asic_family_type"] = self.adl_lib.get_device_asic_family_type
+
+        self.adl_lib.get_device_chipset_info.argtypes = [ctypes.c_int, ctypes.POINTER(ADLChipSetInfo)]
+        self.adl_lib.get_device_chipset_info.restype = ctypes.c_int
+        self.functions["adl_get_device_chipset_info"] = self.adl_lib.get_device_chipset_info
